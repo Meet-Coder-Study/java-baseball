c869d02dcbcbf3ec1a8152c9ecd5c0aa9df02fe2
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-21 00:30:47 +0900

    game controller에 의존성 포함하는 로직 추가

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.example.client.BaseBallGameClient;
 import org.example.client.GameClient;
+import org.example.client.RandomNumberGeneratorImpl;
+import org.example.request.CommandLineRequest;
 
 public class GameController {
 
     public static void main(String[] args) {
-        GameClient gameClient = new BaseBallGameClient();
+        GameClient gameClient = new BaseBallGameClient(new RandomNumberGeneratorImpl(), new CommandLineRequest());
         gameClient.setUp();
         gameClient.play();
     }
