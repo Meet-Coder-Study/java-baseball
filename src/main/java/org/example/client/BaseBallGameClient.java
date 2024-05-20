af61d118265530a088e128d514e4ce72e505e055
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-21 00:29:11 +0900

    game client 의존성 추가

--- src/main/java/org/example/client/BaseBallGameClient.java
+++ src/main/java/org/example/client/BaseBallGameClient.java
@@ -1,10 +1,25 @@
 package org.example.client;
 
+import java.util.List;
+import org.example.request.ClientRequest;
+
 public class BaseBallGameClient implements GameClient {
+
+    private final RandomNumberGenerator randomNumberGenerator;
+    private List<Integer> randomNumbers;
+    private final ClientRequest clientRequest;
+
+    public BaseBallGameClient(
+        RandomNumberGenerator randomNumberGenerator,
+        ClientRequest clientRequest
+    ) {
+        this.randomNumberGenerator = randomNumberGenerator;
+        this.clientRequest = clientRequest;
+    }
 
     @Override
     public void setUp() {
-
+        randomNumbers = randomNumberGenerator.getRandomNumbers();
     }
 
     @Override
