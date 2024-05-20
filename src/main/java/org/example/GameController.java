c1942e85c5eeddadb0a13670460b9f0990db1e94
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-21 00:36:26 +0900

    game controller game box 기준 생성하게 리팩토링

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import org.example.client.BaseBallGameClient;
 import org.example.client.GameClient;
-import org.example.client.RandomNumberGeneratorImpl;
-import org.example.request.CommandLineRequest;
 
 public class GameController {
 
     public static void main(String[] args) {
-        GameClient gameClient = new BaseBallGameClient(new RandomNumberGeneratorImpl(), new CommandLineRequest());
+        GameBox gameBox = new GameBox();
+        GameClient gameClient = gameBox.getGameClient();
         gameClient.setUp();
         gameClient.play();
     }
