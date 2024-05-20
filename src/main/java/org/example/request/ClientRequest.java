2db3fc7b74a9ab9860310b29934bec16e05f7c7d
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-20 23:27:56 +0900

    request 전략패턴 도입

---
+++ src/main/java/org/example/request/ClientRequest.java
@@ -0,0 +1,5 @@
+package org.example.request;
+
+public interface ClientRequest {
+
+}
---
+++ src/main/java/org/example/request/CommandLineRequest.java
@@ -0,0 +1,7 @@
+package org.example.request;
+
+import java.util.Scanner;
+
+public class CommandLineRequest implements ClientRequest {
+    private static final Scanner scanner = new Scanner(System.in);
+}
