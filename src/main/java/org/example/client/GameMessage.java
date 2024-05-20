68970cefa23cd4d0b5e0156495e7b88d342fc9e1
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-21 00:29:19 +0900

    game message 추가

--- src/main/java/org/example/client/GameMessage.java
+++ src/main/java/org/example/client/GameMessage.java
@@ -4,6 +4,13 @@ public class GameMessage {
 
     private GameMessage(){}
 
-    public static final String GAME_START_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.";
+    public static final String GAME_START = "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.";
+    public static final String COMPUTER_CHOOSE_NUMBER = "컴퓨터가 숫자를 뽑았습니다.";
+    public static final String CHOOSE_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String THREE_ALL_COLLECT = "3개의 숫자를 모두 맞히셨습니다.";
+    public static final String GAME_END = "-------게임 종료-------";
+    public static final String NO_CORRECT_NUM = "맞추신 숫자가 없습니다.";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
 
 }
