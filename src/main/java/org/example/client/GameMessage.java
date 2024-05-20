a5116fdd6e732aa011d4b4a50d659f7c4f07362d
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-20 23:28:22 +0900

    게임 메시지 분리

---
+++ src/main/java/org/example/client/GameMessage.java
@@ -0,0 +1,9 @@
+package org.example.client;
+
+public class GameMessage {
+
+    private GameMessage(){}
+
+    public static final String GAME_START_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.";
+
+}
