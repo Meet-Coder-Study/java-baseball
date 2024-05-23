7fc110c500a8460241d2fd49460d8a66d5062dd0
EunjiShin <eunji980310@gmail.com>
2024-05-24 04:43:35 +0900

    test: Answer 테스트 코드 추가

--- java-baseball-meetcoder/src/test/java/org/example/domain/AnswerTest.java
+++ java-baseball-meetcoder/src/test/java/org/example/domain/AnswerTest.java
@@ -1,5 +1,56 @@
 package org.example.domain;
 
-public class AnswerTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class AnswerTest {
+
+    @Test
+    public void 정답을_생성할_수_있다() {
+        // given
+        List<Integer> values = List.of(1, 2, 3);
+
+        // when
+        Answer answer = new Answer(values);
+
+        // then
+        assertEquals(answer.values(), values);
+    }
+
+    @Test
+    public void null로_정답을_생성하려하면_에러를_던진다() {
+        // given
+        List<Integer> values = null;
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Answer(values))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 세자리_자리수가_아닌_수로_정답을_생성하려하면_에러를_던진다() {
+        // given
+        List<Integer> values = List.of(1, 2);
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Answer(values))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 중복값으로_정답을_생성하려하면_에러를_던진다() {
+        // given
+        List<Integer> values = List.of(1, 2, 2);
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Answer(values))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
