13b8ee81b46a92a42c54c69a013b4594fb4a404d
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-24 15:59:31 +0900

    실제 게임이 동작하는 BaseBall GameClient 작성

--- src/main/java/org/example/client/BaseBallGameClient.java
+++ src/main/java/org/example/client/BaseBallGameClient.java
@@ -1,6 +1,7 @@
 package org.example.client;
 
 import java.util.List;
+import org.example.message.GameMessage;
 import org.example.request.ClientRequest;
 
 public class BaseBallGameClient implements GameClient {
@@ -8,6 +9,7 @@ public class BaseBallGameClient implements GameClient {
     private final RandomNumberGenerator randomNumberGenerator;
     private List<Integer> randomNumbers;
     private final ClientRequest clientRequest;
+    private final BaseBallCounter baseBallCounter = new BaseBallCounter();
 
     public BaseBallGameClient(
         RandomNumberGenerator randomNumberGenerator,
@@ -18,18 +20,44 @@ public class BaseBallGameClient implements GameClient {
     }
 
     @Override
-    public void setUp() {
-        randomNumbers = randomNumberGenerator.getRandomNumbers();
+    public int setup() {
+        System.out.println(GameMessage.GAME_START);
+        int gameControlNumber = clientRequest.getNumber();
+        BaseBallValidator.gameStartEndNumberValidator(gameControlNumber);
+        return gameControlNumber;
     }
 
     @Override
     public void play() {
+        System.out.println(GameMessage.COMPUTER_CHOOSE_NUMBER);
+        randomNumbers = randomNumberGenerator.getRandomNumbers();
 
+        System.out.println(GameMessage.CHOOSE_NUMBER);
+        int clientNumber = clientRequest.getNumber();
+
+        BaseBallValidator.validateClientNumber(clientNumber);
+        List<Integer> userInputList = convertingUserNumberToList(clientNumber);
+        BallStrikeCount ballStrikeCount = baseBallCounter.checkCount(randomNumbers, userInputList);
+        ballStrikeCount.showBallStrikeCount();
+        if (ballStrikeCount.strikeCount() != 3) {
+            play();
+            return;
+        }
+
+        System.out.println(GameMessage.GAME_END);
+    }
+
+    private List<Integer> convertingUserNumberToList(int clientNumber) {
+        return List.of(
+            clientNumber / 100,
+            (clientNumber / 10) % 10,
+            clientNumber % 10
+        );
     }
 
     @Override
     public void stop() {
-
+        System.out.println(GameMessage.GAME_END);
     }
 
 }
