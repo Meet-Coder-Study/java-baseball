a54458a3d9806a956757b48eff89e9aaa12c120c
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-24 15:59:15 +0900

    baseball 게임 중 validation을 체크해줄 클래스 작성

---
+++ src/main/java/org/example/client/BaseBallValidator.java
@@ -0,0 +1,38 @@
+package org.example.client;
+
+import java.util.Arrays;
+import java.util.List;
+import org.example.exception.BaseBallException;
+import org.example.message.BaseBallErrorMessage;
+import org.example.message.GameMessage;
+
+public class BaseBallValidator {
+
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 9;
+
+    private BaseBallValidator(){}
+
+    public static void gameStartEndNumberValidator(int number){
+        if(number != START_NUM && number != END_NUM){
+            throw new BaseBallException(BaseBallErrorMessage.INVALID_START_OR_END_NUMBER);
+        }
+    }
+
+    public static void validateClientNumber(int clientNumber) {
+        String stringClientNumber = String.valueOf(clientNumber);
+
+        if(stringClientNumber.length() != 3) {
+            throw new BaseBallException(BaseBallErrorMessage.INVALID_USER_NUMBER_GUESS);
+        }
+
+        long distinctCount = Arrays.stream(stringClientNumber.split(""))
+            .distinct()
+            .count();
+
+        if(distinctCount !=3 ){
+            throw new BaseBallException(BaseBallErrorMessage.INVALID_USER_NUMBER_GUESS);
+        }
+    }
+
+}
