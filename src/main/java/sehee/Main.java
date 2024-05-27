56c392828f96cd19c7b18e77ba4d77af1c517a28
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-05-27 16:47:52 +0900

    init: 초기 세팅

--- src/main/java/sehee/Main.java
+++ src/main/java/sehee/Main.java
@@ -1,9 +1,34 @@
 package sehee;
 
+import sehee.domain.answer.AnswerFactory;
+import sehee.domain.hint.HintProvider;
+import sehee.exception.ExceptionHandler;
+import sehee.game.Game;
+import sehee.game.GamePlayer;
+import sehee.game.RandomNumberGame;
+import sehee.io.in.CliReader;
+import sehee.io.in.Reader;
+import sehee.io.out.CliPrinter;
+import sehee.io.out.Printer;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // IO
+        Reader reader = new CliReader();
+        Printer printer = new CliPrinter();
+
+        // Game
+        AnswerFactory answerFactory = new AnswerFactory();
+        HintProvider hintProvider = new HintProvider();
+        Game game = new RandomNumberGame(reader, printer, answerFactory, hintProvider);
+
+        // Exception
+        ExceptionHandler exceptionHandler = new ExceptionHandler(printer);
+
+        // Play Game
+        GamePlayer gamePlayer = new GamePlayer(reader, printer, game, exceptionHandler);
+        gamePlayer.on();
     }
 
 }
