8ca4fb0b1bbda3388b24e3b7ed3025c188937d54
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-05-28 14:02:25 +0900

    feat: GamePlayer 리팩토링

--- src/main/java/sehee/game/GamePlayer.java
+++ src/main/java/sehee/game/GamePlayer.java
@@ -5,25 +5,36 @@ import sehee.exception.ExceptionHandler;
 import sehee.io.in.Reader;
 import sehee.io.out.Printer;
 
-public record GamePlayer(Reader reader, Printer printer, Game game, ExceptionHandler exceptionHandler) {
+public record GamePlayer(
+    Reader reader,
+    Printer printer,
+    Game game,
+    ExceptionHandler exceptionHandler
+) {
 
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 9;
 
     public void on() {
-        while (true) {
-            try {
-                if (stopGame()) {
-                    break;
-                }
+        boolean working = true;
+        while (working) {
+            working = checkPlayingAndPlayGame();
+        }
+        printer.println("애플리케이션이 종료되었습니다.");
+    }
 
-                game.play();
-            } catch (Exception exception) {
-                exceptionHandler.alert(exception);
+    private boolean checkPlayingAndPlayGame() {
+        try {
+            if (stopGame()) {
+                return false;
             }
+
+            game.play();
+        } catch (Exception exception) {
+            exceptionHandler.alert(exception);
         }
 
-        printer.println("애플리케이션이 종료되었습니다.");
+        return true;
     }
 
     private boolean stopGame() throws IOException {
