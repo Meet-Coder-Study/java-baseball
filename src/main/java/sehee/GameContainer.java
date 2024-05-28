a45d6078065495be85cce65cda4c7685495bc494
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-05-28 15:58:12 +0900

    rename: 클래스명 변경

--- src/main/java/sehee/GameContainer.java
+++ src/main/java/sehee/GameContainer.java
@@ -4,15 +4,16 @@ import java.util.Random;
 import sehee.answer.AnswerFactory;
 import sehee.exception.ExceptionHandler;
 import sehee.game.Game;
-import sehee.game.GamePlayer;
 import sehee.game.RandomNumberGame;
+import sehee.game.player.GamePlayer;
 import sehee.io.in.CliReader;
 import sehee.io.in.Reader;
 import sehee.io.out.CliPrinter;
 import sehee.io.out.Printer;
-import sehee.answer.NumberMaker;
+import sehee.util.numbermaker.NumberMaker;
+import sehee.util.numbermaker.RandomNumberMaker;
 
-public class Main {
+public class GameContainer {
 
     public static void main(String[] args) {
         // IO
@@ -23,7 +24,7 @@ public class Main {
         ExceptionHandler exceptionHandler = new ExceptionHandler(printer);
 
         // Set Game
-        NumberMaker numberMaker = new NumberMaker(new Random());
+        NumberMaker numberMaker = new RandomNumberMaker(new Random());
         AnswerFactory answerFactory = new AnswerFactory(numberMaker);
         Game game = new RandomNumberGame(reader, printer, answerFactory, exceptionHandler);
 
