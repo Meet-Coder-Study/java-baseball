9f7b7e84e66ac73d32afbe3c4b20f6fbe44e1e97
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-05-28 15:16:03 +0900

    refactor: ReadConstant 래팩토링

--- src/main/java/sehee/io/in/ReadConstant.java
+++ src/main/java/sehee/io/in/ReadConstant.java
@@ -3,7 +3,7 @@ package sehee.io.in;
 // package-private
 final class ReadConstant {
 
-    public static final char CHAR_ZERO = '0';
+    static final char CHAR_ZERO = '0';
     static final int ASCII_CODE_DECIMAL_ONE = 49;
     static final int ASCII_CODE_DECIMAL_NINE = 57;
 
